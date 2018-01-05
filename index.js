61a63953af6ac7489af4bc70abfaf42b23d593b6
qpython1000100 <dphilippus123@gmail.com>
2018-01-05 15:50:30 -0700

    Popup, at least, is working

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var pairing = require("pairing");
+var bookmark = require("bookmark");
 
 var set_key = pairing.set_key
+var bookmark = function() {
+    console.log("Running scrape");
+    pairing.do_bookmark();
+}
